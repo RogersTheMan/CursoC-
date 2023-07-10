0825ca311d64e7e8b22748d6a70584ba017582cb
Rodrigo Pretel <pretrod971@gmail.com>
2023-07-10 08:32:23 -0300

    fixing 14

--- 14interface/Calculo.cs
+++ 14interface/Calculo.cs
@@ -4,12 +4,22 @@ class Calculo : IPadrao
 {
     public void somar(double n1, double n2)
     {
-        Console.WriteLine("A soma é "+(n1+n2));
+        Console.WriteLine("A soma entre "+n1+" e "+n2+" é = "+(n1+n2));
     }
 
     public void sub(double n1,double n2)
     {
-        Console.WriteLine("A sub é "+(n1-n2));
+        Console.WriteLine("A sub entre "+n1+" e "+n2+" é = " +(n1-n2));
 
+    }
+
+    public void mult(double n1, double n2)
+    {
+        Console.WriteLine("A mult entre "+n1+" e "+n2+" é = "+(n1*n2));
+    }
+
+    public void div(double n1, double n2)
+    {
+        Console.WriteLine("A div entre "+n1+" e "+n2+" é = "+(n1/n2));
     }
 }
--- 14interface/IPadrao.cs
+++ 14interface/IPadrao.cs
@@ -5,4 +5,9 @@ interface IPadrao
 
     void sub(double n1, double n2);
 
+    void mult(double n1, double n2);
+
+    void div(double n1, double n2);
+     
+
 }
